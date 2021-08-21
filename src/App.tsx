391924683756c7ac8ea1a35060d431f32a40de62
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-08-21 09:53:35 +0700

    add router 🗺

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React, { FC } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom'
 
-const App: FC = () => {
+const useQuery = () => new URLSearchParams(useLocation().search)
+
+const App: FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-content">
-        <p>Please choose your desire!</p>
-        <p>
-          <button type="button">Search Your Giphy</button>
-          <button type="button">Iron Man Giphy</button>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className="row justify-content-md-center">
+          <p className="col-6 text-center">Please choose your Giphy Mode!</p>
+        </div>
+        <div className="row justify-content-md-center">
+          <div className="text-center">
+            <Link to="/search" className="btn btn-primary m-2">Search Your Giphy</Link>
+            <Link to="/search?q=iron%20man" className="btn btn-primary m-2">Iron Man Giphy</Link>
+          </div>
+        </div>
+      </div>
+    </Router>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './public/index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './public/style.css'
+
 import App from './App'
 
 ReactDOM.render(
